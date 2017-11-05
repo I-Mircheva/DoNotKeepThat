98a0d87fba54f82cfcb1b10a40236fc5e375e210
Ivona <ivona1mircheva@gmail.com>
2017-11-05 10:58:46 +0200

    Added page for showing urls without actual message

--- app/controllers/hidden_notes_controller.rb
+++ app/controllers/hidden_notes_controller.rb
@@ -1,5 +1,5 @@
 class HiddenNotesController < ApplicationController
-  before_action :set_hidden_note, only: [:show, :edit, :update, :destroy]
+  before_action :set_hidden_note, only: [:show, :destroy]
 
   # GET /hidden_notes
   # GET /hidden_notes.json
@@ -10,15 +10,19 @@ class HiddenNotesController < ApplicationController
   # GET /hidden_notes/1
   # GET /hidden_notes/1.json
   def show
+     @hidden_note.destroy
+    respond_to do |format|
+      format.html { render :show, notice: 'This Hidden note is now successfully destroyed.'}
+      format.json { head :no_content }
+    end
+  end
+  # i made dis
+  def show_url
   end
 
   # GET /hidden_notes/new
   def new
     @hidden_note = HiddenNote.new
-  end
-
-  # GET /hidden_notes/1/edit
-  def edit
   end
 
   # POST /hidden_notes
@@ -28,24 +32,10 @@ class HiddenNotesController < ApplicationController
 
     respond_to do |format|
       if @hidden_note.save
-        format.html { redirect_to @hidden_note, notice: 'Hidden note was successfully created.'}
-        format.json { render :show, status: :created, location: @hidden_note }
+        format.html { render :show_url, notice: 'Hidden note was successfully created.'}
+        format.json { render :show_url, status: :created, location: @hidden_note }
       else
         format.html { render :new }
-        format.json { render json: @hidden_note.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /hidden_notes/1
-  # PATCH/PUT /hidden_notes/1.json
-  def update
-    respond_to do |format|
-      if @hidden_note.update(hidden_note_params)
-        format.html { redirect_to @hidden_note, notice: 'Hidden note was successfully updated.' }
-        format.json { render :show, status: :ok, location: @hidden_note }
-      else
-        format.html { render :edit }
         format.json { render json: @hidden_note.errors, status: :unprocessable_entity }
       end
     end
@@ -64,7 +54,8 @@ class HiddenNotesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_hidden_note
-      @hidden_note = HiddenNote.find(params[:id])
+      @hidden_note = HiddenNote.find_by(id: params[:id])
+
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
